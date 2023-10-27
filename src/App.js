9c155cd6b9d440d66c94d338fb70f5d13d9f4703
raghul03032002 <140879444+raghul03032002@users.noreply.github.com>
2023-10-27 11:50:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Weather } from "./Component/Weather";
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Weather/>
+    </div>
+  );
+}
+
+export default App;
